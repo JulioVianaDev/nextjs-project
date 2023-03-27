639ed936152f56457ad323641a75910fe820fdfd
juliovianadev <juliovianadev@gmail.com>
2023-03-27 17:10:54 -0300

    criando a primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import MeetupList from '../components/meetups/MeetupList';
 
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: "uma imagem ai",
+    image: "https://static.independent.co.uk/2023/03/07/15/newFile-3.jpg",
+    address: 'algum endereço',
+    description: "uma descrição ai"
+  },
+  {
+    id: 'm2',
+    title: "uma segunda imagem ai",
+    image: "https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-paris-capa2019-02.jpg",
+    address: 'algum endereço',
+    description: "uma descrição ai"
+  },
+]
 function HomePage() {
   return (
-    <div>HomePage</div>
+   <MeetupList meetups={DUMMY_MEETUPS}/>
   )
 }
 
