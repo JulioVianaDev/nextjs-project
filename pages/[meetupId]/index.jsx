43672b4c86b98c9c7c71a90675608329685b190c
juliovianadev <juliovianadev@gmail.com>
2023-03-27 22:15:48 -0300

    detail page criada

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,16 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+function MeetupDetails(){
+  return(
+    <>
+      <MeetupDetail
+        title='titulo'
+        address='endereço'
+        image="https://static.independent.co.uk/2023/03/07/15/newFile-3.jpg"
+        description='decriçãp'
+      />
+    </>
+  )
+}
+
+export default MeetupDetails
