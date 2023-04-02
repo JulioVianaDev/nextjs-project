8b9ee0d8bc6b4db9058932868018dce0fedee478
juliovianadev <juliovianadev@gmail.com>
2023-04-02 11:34:43 -0300

    conectando no mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,22 @@
-function handler(req,res){
+import { MongoClient } from "mongodb";
+
+
+async function handler(req,res){
   if (req.method === 'POST'){
     const data = req.body;
-    const {title,image,address,description} = data;
+    const client = await MongoClient.connect('mongodb+srv://<tomate>:<tomate69>@react.1fwaoej.mongodb.net/meetups?retryWrites=true&w=majority');
+    
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    
+    const result = await meetupsCollection.insertOne(data);
+    
+    console.log(result)
+  
+    client.close();
+
+    res.status(201).json({message: 'meetup inserido com sucesso'})
   }
 }
 
